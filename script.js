cd35ba870405324c4f19af8fd72d2bb44ecac12e
Kushagra <kushagra24august@gmail.com>
2022-04-09 15:36:59 +0530

    no details

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 const btRock = document.getElementById("rock");
 const btPaper = document.getElementById("paper");
 const btScissor = document.getElementById("scissor");
-let randomNumber = Math.floor(Math.random() * 3) + 1;
+const randomNumber = Math.floor(Math.random() * 3) + 1;
 
 
 btRock.addEventListener("click", e => {
-    document.getElementById("user").innerHTML = "Rock chosen";
+    document.getElementById("user").innerHTML = "You chose Rock";
+
+
     if(randomNumber == 1) {
-        document.getElementById("comp").innerHTML = "Computer chose rock";
+        document.getElementById("comp").innerHTML = "Computer chose Rock";
+        document.getElementById("final").innerHTML = "DRAW";
     }
     else if(randomNumber == 2) {
-        document.getElementById("comp").innerHTML = "Computer chose paper";
+        document.getElementById("comp").innerHTML = "Computer chose Paper";
+        document.getElementById("final").innerHTML = "YOU LOSE";
     }
     else {
-        document.getElementById("comp").innerHTML = "Computer chose scissor";
+        document.getElementById("comp").innerHTML = "Computer chose Scissor";
+        document.getElementById("final").innerHTML = "YOU WIN";
+    }
+})
+btPaper.addEventListener("click", e => {
+    document.getElementById("user").innerHTML = "You chose Paper";
+
+
+    if(randomNumber == 1) {
+        document.getElementById("comp").innerHTML = "Computer chose Rock";
+        document.getElementById("final").innerHTML = "You WIN";
+    }
+    else if(randomNumber == 2) {
+        document.getElementById("comp").innerHTML = "Computer chose Paper";
+        document.getElementById("final").innerHTML = "DRAW";
+    }
+    else {
+        document.getElementById("comp").innerHTML = "Computer chose Scissor";
+        document.getElementById("final").innerHTML = "You LOSE";
+    }
+})
+btScissor.addEventListener("click", e=> {
+    document.getElementById("user").innerHTML = "You chose Scissor";
+
+    if(randomNumber == 1) {
+        document.getElementById("comp").innerHTML = "Computer chose Rock";
+        document.getElementById("final").innerHTML = "You LOSE";
+    }
+    else if(randomNumber == 2) {
+        document.getElementById("comp").innerHTML = "Computer chose Paper";
+        document.getElementById("final").innerHTML = "You WIN";
+    }
+    else {
+        document.getElementById("comp").innerHTML = "Computer chose Scissor";
+        document.getElementById("final").innerHTML = "DRAW";
     }
 })
