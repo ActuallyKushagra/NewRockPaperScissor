8ef8380b73326918168d9d35122251c4d9531d98
Kushagra <kushagra24august@gmail.com>
2022-04-11 22:03:24 +0530

    final

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const btRock = document.getElementById("rock");
 const btPaper = document.getElementById("paper");
 const btScissor = document.getElementById("scissor");
-const randomNumber = Math.floor(Math.random() * 3) + 1;
+
 
 
 btRock.addEventListener("click", e => {
     document.getElementById("user").innerHTML = "You chose Rock";
-
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
 
     if(randomNumber == 1) {
         document.getElementById("comp").innerHTML = "Computer chose Rock";
@@ -23,11 +23,11 @@ btRock.addEventListener("click", e => {
 })
 btPaper.addEventListener("click", e => {
     document.getElementById("user").innerHTML = "You chose Paper";
-
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
 
     if(randomNumber == 1) {
         document.getElementById("comp").innerHTML = "Computer chose Rock";
-        document.getElementById("final").innerHTML = "You WIN";
+        document.getElementById("final").innerHTML = "YOU WIN";
     }
     else if(randomNumber == 2) {
         document.getElementById("comp").innerHTML = "Computer chose Paper";
@@ -35,22 +35,27 @@ btPaper.addEventListener("click", e => {
     }
     else {
         document.getElementById("comp").innerHTML = "Computer chose Scissor";
-        document.getElementById("final").innerHTML = "You LOSE";
+        document.getElementById("final").innerHTML = "YOU LOSE";
     }
 })
 btScissor.addEventListener("click", e=> {
     document.getElementById("user").innerHTML = "You chose Scissor";
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
 
     if(randomNumber == 1) {
         document.getElementById("comp").innerHTML = "Computer chose Rock";
-        document.getElementById("final").innerHTML = "You LOSE";
+        document.getElementById("final").innerHTML = "YOU LOSE";
     }
     else if(randomNumber == 2) {
         document.getElementById("comp").innerHTML = "Computer chose Paper";
-        document.getElementById("final").innerHTML = "You WIN";
+        document.getElementById("final").innerHTML = "YOU WIN";
     }
     else {
         document.getElementById("comp").innerHTML = "Computer chose Scissor";
         document.getElementById("final").innerHTML = "DRAW";
     }
+})
+const rel = document.getElementById("rst");
+rel.addEventListener("click", e=> {
+    location.reload();
 })
